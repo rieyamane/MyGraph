e36d460e2633b71c95038fa844e233a5e7b604ab
rieyamane <rieyamane@gmail.com>
2011-11-10 22:17:12 +0900

    third commit

---
+++ MyGraph/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyGraph
+//
+//  Created by 山根 理恵 on 11/11/05.
+//  Copyright (c) 2011年 Martian77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
