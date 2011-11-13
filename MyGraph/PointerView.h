3c297aade3726dd0b134336d61428073b971db69
rieyamane <rieyamane@gmail.com>
2011-11-13 13:03:53 +0900

    PointerView追加

---
+++ MyGraph/PointerView.h
@@ -0,0 +1,13 @@
+//
+//  PointerView.h
+//  MyGraph
+//
+//  Created by 山根 理恵 on 11/11/13.
+//  Copyright (c) 2011年 Martian77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PointerView : UIView
+
+@end
---
+++ MyGraph/PointerView.m
@@ -0,0 +1,31 @@
+//
+//  PointerView.m
+//  MyGraph
+//
+//  Created by 山根 理恵 on 11/11/13.
+//  Copyright (c) 2011年 Martian77. All rights reserved.
+//
+
+#import "PointerView.h"
+
+@implementation PointerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
