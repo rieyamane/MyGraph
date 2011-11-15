478ce12ba422cb22baafe08be7ed2d74eb012ec8
rieyamane <rieyamane@gmail.com>
2011-11-15 20:53:03 +0900

    chapter5:draw textまで進んだ

--- MyGraph/GraphView.m
+++ MyGraph/GraphView.m
@@ -16,6 +16,8 @@ float data[] = {0.7, 0.4, 0.9, 1.0, 0.2, 0.85, 0.11, 0.75, 0.53, 0.44, 0.88, 0.7
 //float data[] = {135, 134, 142, 143, 139, 145, 139, 145, 139, 137, 136, 142, 152, 133};
 /**************************/
 
+CGRect touchAreas[kNumberOfBars];
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -99,6 +101,75 @@ float data[] = {0.7, 0.4, 0.9, 1.0, 0.2, 0.85, 0.11, 0.75, 0.53, 0.44, 0.88, 0.7
     CGContextDrawPath(ctx, kCGPathFillStroke);
 }
 
+// Draw bar graph itself
+
+- (void)drawBar:(CGRect)rect context:(CGContextRef)ctx 
+{
+    // Prepare the resources
+    CGFloat components[12] = {0.2314, 0.5686, 0.4, 1.0,  // Start color
+        0.4727, 1.0, 0.8157, 1.0, // Second color
+        0.2392, 0.5686, 0.4118, 1.0}; // End color
+    CGFloat locations[3] = {0.0, 0.33, 1.0};
+    size_t num_locations = 3;
+    
+	CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorspace, components, locations, num_locations);
+    
+	CGPoint startPoint = rect.origin;
+	CGPoint endPoint = CGPointMake(rect.origin.x + rect.size.width, rect.origin.y);
+    
+    // Create and apply the clipping path
+    CGContextBeginPath(ctx);
+	CGContextSetGrayFillColor(ctx, 0.2, 0.7);
+	CGContextMoveToPoint(ctx, CGRectGetMinX(rect), CGRectGetMinY(rect));
+    CGContextAddLineToPoint(ctx, CGRectGetMaxX(rect), CGRectGetMinY(rect));
+	CGContextAddLineToPoint(ctx, CGRectGetMaxX(rect), CGRectGetMaxY(rect));
+	CGContextAddLineToPoint(ctx, CGRectGetMinX(rect), CGRectGetMaxY(rect));
+    CGContextClosePath(ctx);
+    
+    CGContextSaveGState(ctx);
+	CGContextClip(ctx);
+    
+    // Draw the gradient
+	CGContextDrawLinearGradient(ctx, gradient, startPoint, endPoint, 0);
+    CGContextRestoreGState(ctx);
+    
+    // Release the resources
+    CGColorSpaceRelease(colorspace);
+	CGGradientRelease(gradient);
+}
+
+// Draw bar graph
+
+- (void)drawBarGraphWithContext:(CGContextRef)ctx
+{
+    // Draw the bars
+    float maxBarHeight = kGraphHeight - kBarTop - kOffsetY;
+//    for (int i = 0; i < sizeof(data); i++)
+    for (int i = 0; i < kNumberOfBars; i++)
+    {
+        float barX = kOffsetX + kStepX + i * kStepX - kBarWidth / 2;
+        float barY = kBarTop + maxBarHeight - maxBarHeight * data[i];
+        float barHeight = maxBarHeight * data[i];
+        CGRect barRect = CGRectMake(barX, barY, kBarWidth, barHeight);
+        [self drawBar:barRect context:ctx];
+        touchAreas[i] = barRect;
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
+    NSLog(@"Touch x:%f, y:%f", point.x, point.y);
+    
+    for (int i = 0; i < kNumberOfBars; i++) {
+        if (CGRectContainsPoint(touchAreas[i], point)) {
+            NSLog(@"Tapped a bar with index %d, value %f", i,data[i]);
+            break;
+        }
+    }
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
@@ -127,7 +198,8 @@ float data[] = {0.7, 0.4, 0.9, 1.0, 0.2, 0.85, 0.11, 0.75, 0.53, 0.44, 0.88, 0.7
     CGContextStrokePath(context);
     CGContextSetLineDash(context, 0, NULL, 0); // Remove the dash
     
-    [self drawLineGraphWithContext:context];
+//    [self drawLineGraphWithContext:context];
+    [self drawBarGraphWithContext:context];
     
 }
 
